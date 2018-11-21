235ca5d6bb1e40839ebc519e257bb51a17973117
Travis Cooper <dozertdc@gmail.com>
2018-11-21 12:05:40 -0500

    Update Form.java

--- Form.java
+++ Form.java
@@ -1,5 +1,12 @@
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Form {
 
@@ -20,6 +27,12 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
 
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+
+        String alertText = alert.getText();
+
+        assertEquals("The form was successfully submitted!", alertText);
 
         driver.quit();
 
