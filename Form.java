5cdc81ed092247293b094e2a94276d293a89d2ad
Travis Cooper <dozertdc@gmail.com>
2018-11-21 13:01:02 -0500

    Update Form.java

--- Form.java
+++ Form.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -17,6 +16,18 @@ public class Form {
 
         driver.get("https://formy-project.herokuapp.com/form");
 
+        submitForm(driver);
+
+        waitForAlertBanner(driver);
+
+        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+
+        driver.quit();
+
+    }
+
+    public static void submitForm(WebDriver driver){
+
         driver.findElement(By.id("first-name")).sendKeys("Travis");
         driver.findElement(By.id("last-name")).sendKeys("Cooper");
         driver.findElement(By.id("job-title")).sendKeys("Software Engineer");
@@ -27,14 +38,19 @@ public class Form {
         driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
         driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
 
+    }
+
+    public static void waitForAlertBanner(WebDriver driver){
+
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
 
-        String alertText = alert.getText();
+    }
 
-        assertEquals("The form was successfully submitted!", alertText);
+    public static String getAlertBannerText(WebDriver driver){
 
-        driver.quit();
+        String alertText = driver.findElement(By.className("alert")).getText();
+        return alertText;
 
     }
 
